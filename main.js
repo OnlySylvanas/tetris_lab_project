2a26fcbf5cd93ea735a5b6a368be1a754bb3bddf
OnlySylvanas <onlysylvanas@yandex.ru>
2023-03-04 14:55:50 +0300

    initialize all required canvases

---
+++ main.js
@@ -0,0 +1,4 @@
+let gameBoard = document.getElementById('board');
+let gameBoardContext = gameBoard.getContext('2d');
+let nextBlockPresenter = document.getElementById('next_block_presenter');
+let nextBlockPresenterContext = nextBlockPresenter.getContext('2d');
