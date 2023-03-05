8197749ad936c5a4c2e5e86ab96c651b9bbe7f13
OnlySylvanas <onlysylvanas@yandex.ru>
2023-03-05 15:08:19 +0300

    add ui management functionality

--- main.js
+++ main.js
@@ -3,6 +3,26 @@ let gameBoardContext = gameBoard.getContext('2d');
 let nextBlockPresenter = document.getElementById('next_block_presenter');
 let nextBlockPresenterContext = nextBlockPresenter.getContext('2d');
 
+let gameState = {
+	score: 0,
+	level: 0,
+	lines: 0
+};
+
+function updateGameState(key, value) {
+	let uiLabel = document.getElementById(key);
+	if (uiLabel)
+		uiLabel.textContent = value
+}
+
+let gameStateUpdateHook = new Proxy(gameState, {
+	set(target, key, value) {
+		target[key] = value;
+		updateGameState(key, value);
+		return true;
+	}
+})
+
 let board = new GameBoard(gameBoardContext, nextBlockPresenterContext);
 initializeNextBlockPresenter();
 
